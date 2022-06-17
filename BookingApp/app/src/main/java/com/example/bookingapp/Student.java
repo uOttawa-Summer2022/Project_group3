39611a1dad7bee31a8640798d243658cb5b5b1c0
eyaco078 <eyaco078@uottawa.ca>
2022-06-17 16:48:51 -0400

    added models + accounts Database still Course Database sql lite

---
+++ BookingApp/app/src/main/java/com/example/bookingapp/Student.java
@@ -0,0 +1,7 @@
+package com.example.bookingapp;
+
+public class Student extends Account{
+    public Student(String user_name, String password, Enum type) {
+        super(user_name, password, type);
+    }
+}
